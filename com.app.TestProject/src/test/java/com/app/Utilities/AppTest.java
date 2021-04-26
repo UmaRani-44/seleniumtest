6dab8e45c5a67feaaee16f6bc75cc793c6cd0df2
vamshi <vamshi@DESKTOP-6CIC8LT>
2021-04-26 16:17:53 +0530

    First Commit

---
+++ com.app.TestProject/src/test/java/com/app/Utilities/AppTest.java
@@ -0,0 +1,18 @@
+package com.app.Utilities;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void verifyTest()
+    {
+        System.out.println("Selenium Test");
+    }
+}
